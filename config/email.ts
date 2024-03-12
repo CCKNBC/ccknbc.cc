46f788a8d9cd73a8f369913dd1811c0bc9c812b9
CCKNBC <ccknbc@qq.com>
2024-03-12 23:55:29 +0800

    feat: refactor planetscale to neon

--- config/email.ts
+++ config/email.ts
@@ -1,7 +1,9 @@
+import { env } from '~/env.mjs'
+
 export const emailConfig = {
-  from: 'cc@ccknbc.cc',
+  from: env.NEXT_PUBLIC_SITE_EMAIL_FROM,
   baseUrl:
-    process.env.VERCEL_ENV === 'production'
-      ? `https://www.ccknbc.cc`
+    env.VERCEL_ENV === 'production'
+      ? env.NEXT_PUBLIC_SITE_URL
       : 'http://localhost:3000',
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { get } from '@vercel/edge-config'
-
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
@@ -8,20 +6,55 @@ import { get } from '@vercel/edge-config'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-
   images: {
-    domains: ['cdn.sanity.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/**`,
+      }
+    ],
   },
 
-  async redirects() {
-    try {
-      return (await get('redirects')) ?? []
-    } catch {
-      return []
-    }
+  redirects() {
+    return [
+      {
+        "source": "/twitter",
+        "destination": "https://x.com/ccknbc",
+        "permanent": true
+      },
+      {
+        "source": "/x",
+        "destination": "https://x.com/ccknbc",
+        "permanent": true
+      },
+      {
+        "source": "/youtube",
+        "destination": "https://youtube.com/@ccknbc",
+        "permanent": true
+      },
+      {
+        "source": "/tg",
+        "destination": "https://t.me/ccknbc_bot",
+        "permanent": true
+      },
+      {
+        "source": "/linkedin",
+        "destination": "https://www.linkedin.com/in/calicastle/",
+        "permanent": true
+      },
+      {
+        "source": "/github",
+        "destination": "https://github.com/CCKNBC",
+        "permanent": true
+      },
+      {
+        "source": "/bilibili",
+        "destination": "https://space.bilibili.com/1404413254",
+        "permanent": true
+      }
+    ]
   },
 
   rewrites() {
